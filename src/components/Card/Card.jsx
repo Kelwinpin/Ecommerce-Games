145b11114798f9041a36d033087805d2842ff6ed
Kelwin.pin <Kelwincomw@gmail.com>
2023-02-05 18:03:48 -0300

    Adicionando Cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,18 @@
-import styles from './Card.Module.css'
+import styles from './Card.module.css'
 
-export const Card = () =>{
-    return(
-        <div>
-            
+
+export const Card = ({imagem, descricao, valor}) => {
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardImage}>
+                <img src={imagem} alt={`Foto de ${descricao}`} />
+            </div>
+            <div>
+                <h5 className={styles.descricao}>{descricao}</h5>
+                <h2>R${valor}</h2>
+            </div>
         </div>
     )
 }
+
+export default Card;
--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,9 +1,18 @@
+import styles from './Inicio.module.css'
+import Card from "components/Card";
+import foto from 'image/GBA.png'
 
 export const Inicio = () => {
     return (
-        <>
-   
-        </>
+        <section>
+            <div className={styles.fileiraCards}>
+                <Card descricao="Console Gameboy Advanced" imagem={foto} valor='350,00'/>
+                <Card descricao="Console Gameboy Advanced" imagem={foto} valor='350,00'/>
+                <Card descricao="Console Gameboy Advanced" imagem={foto} valor='350,00'/>
+                <Card descricao="Console Gameboy Advanced" imagem={foto} valor='350,00'/>
+                <Card descricao="Console Gameboy Advanced" imagem={foto} valor='350,00'/>
+            </div>
+        </section>
     )
 }
 
